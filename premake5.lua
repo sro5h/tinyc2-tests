92b40946ea2b58b5c14c42687c4a3f1d99d8c3c1
Paul Meffle <meffle.paul@me.com>
2017-09-07 21:03:29 +0200

    Simplify the structure and premake file

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+-- Make gmake the default action
+if not _ACTION then
+        _ACTION = "gmake"
+end
+
+workspace "Tests"
+        configurations { "Debug" }
+        kind "ConsoleApp"
+        language "C++"
+        targetdir "build"
+        includedirs { "." }
+
+        filter "configurations:Debug"
+                defines { "_DEBUG" }
+                symbols "On"
+                links { "sfml-system", "sfml-window", "sfml-graphics" }
+
+project "AabbToPolygon"
+        files { "aabb/AabbToPolygon.cpp" }
+
+project "CircleToCircle"
+        files { "circle/CircleToCircle.cpp" }
+
+project "CircleToPolygon"
+        files { "circle/CircleToPolygon.cpp" }
+
+project "RayToAabb"
+        files { "ray/RayToAabb.cpp" }
+
+project "RayToCircle"
+        files { "ray/RayToCircle.cpp" }
+
+project "RayToPolygon"
+        files { "ray/RayToPolygon.cpp" }
+
+-- Removes all generated and compiled files
+newaction {
+        trigger = "clean",
+        description = "Clean the software.",
+        execute = function ()
+                os.rmdir("./build")
+                os.rmdir("./bin")
+                os.rmdir("./obj")
+                os.remove("./Makefile")
+                os.remove("./*.make")
+                print("Done.")
+        end
+}
